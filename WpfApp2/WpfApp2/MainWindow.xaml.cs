9a832407e60dd74529c29ab4d2ab7183faa98624
DevDominik <71924117+DevDominik@users.noreply.github.com>
2023-05-18 14:26:31 +0200

    Add files via upload

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Szakasz> szakaszok;
+        public MainWindow()
+        {
+            InitializeComponent();
+            szakaszok = File.ReadAllLines("kektura.txt").Select(obj => new Szakasz(obj)).ToList();
+            dgAdatok.ItemsSource = szakaszok;
+        }
+
+        private void btnSzakasz_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Szakaszok mennyisége: {szakaszok.Count()}", "Szakasz");
+        }
+
+        private void btnListaz_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbSzam.Text.Trim().Length < 1)
+            {
+                MessageBox.Show("Nincs szám megadva");
+                return;
+            }
+            try
+            {
+                double a = double.Parse(tbSzam.Text.Trim());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nem helyes a bevitt szám");
+                tbSzam.Focus();
+                throw;
+            }
+            List<Szakasz> temp = szakaszok.Where(x => x.Idotartam <= double.Parse(tbSzam.Text.Trim())).ToList();
+            dgAdatok.ItemsSource = temp;
+            dgAdatok.Items.Refresh();
+            lblListazottMennyiseg.Content = temp.Count();
+        }
+
+        private void btnPilisAtlag_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Átlagos időtartam: {szakaszok.Where(x => x.Nev.Contains("Pilis")).ToList().Average(x => x.Idotartam)}", "Pilis Átlag");
+        }
+        private void btnVissza_Click(object sender, RoutedEventArgs e)
+        {
+            dgAdatok.ItemsSource = szakaszok;
+            dgAdatok.Items.Refresh();
+            lblListazottMennyiseg.Content = "n/a";
+        }
+
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            Szakasz leghosszabb = szakaszok[0];
+            foreach (Szakasz item in szakaszok)
+            {
+                if (leghosszabb.Tavolsag < item.Tavolsag)
+                {
+                    leghosszabb = item;
+                }
+            }
+            MessageBox.Show($"{leghosszabb.Nev} - {leghosszabb.Tavolsag} km - {leghosszabb.Idotartam} perc", "Max");
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/Szakasz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class Szakasz
+    {
+        string nev;
+        double km, tengerszint, perc;
+        string nehezseg;
+        public Szakasz(string bevitel)
+        {
+            string[] adatok = bevitel.Split(';');
+            this.nev = adatok[0];
+            this.km = double.Parse(adatok[1]);
+            this.tengerszint = double.Parse(adatok[2]);
+            this.perc = double.Parse(adatok[3]);
+            if (this.km < 5)
+            {
+                this.nehezseg = "Könnyű";
+            }
+            else if (this.km >= 5 && this.km <= 10)
+            {
+                this.nehezseg = "Közepes";
+            }
+            else if (this.km > 10)
+            {
+                this.nehezseg = "Nehéz";
+            }
+        }
+
+        public string Nev { get { return nev; } }
+        public double Tavolsag { get { return km; } }
+        public double Tengerszint { get { return tengerszint; } }
+        public double Idotartam { get { return perc; } }
+        public string Nehezseg { get { return nehezseg; } }
+
+    }
+}
